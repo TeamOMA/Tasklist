138b8d783e9f2736f622ec60a634b6a1148d388e
Oren Amsalem <oamsalem@gmail.com>
2015-06-04 14:28:31 +0300

    Add test to project

---
+++ src/test/java/TaskListResourceTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TaskListResourceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testTaskListResource() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSayTask() {
+		fail("Not yet implemented");
+	}
+
+}
